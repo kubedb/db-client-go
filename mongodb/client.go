6eb26b59d02e251ed91ca670b42538bf076d46e2
Mohammad Fahim Abrar <fahimabrar@appscode.com>
2021-11-02 16:01:33 +0600

    Add mongodb close client
    Signed-off-by: Mohammad Fahim Abrar <fahimabrar@appscode.com>

--- mongodb/client.go
+++ mongodb/client.go
@@ -17,9 +17,20 @@ limitations under the License.
 package mongodb
 
 import (
+	"context"
+
 	"go.mongodb.org/mongo-driver/mongo"
+	"k8s.io/klog/v2"
 )
 
 type Client struct {
 	*mongo.Client
+}
+
+func (c *Client) Close() {
+	err := c.Disconnect(context.TODO())
+	if err != nil {
+		klog.Errorf("Failed to disconnect client. error: %v", err)
+		return
+	}
 }
