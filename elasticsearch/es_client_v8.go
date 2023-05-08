9f6bae35f379066e56eff24f73e642da5a114047
raihankhan <raihan@appscode.com>
2023-05-08 18:37:51 +0600

    Use Mi instead of Gi
    Signed-off-by: raihankhan <raihan@appscode.com>

--- elasticsearch/es_client_v8.go
+++ elasticsearch/es_client_v8.go
@@ -271,8 +271,8 @@ func (es *ESClientV8) GetTotalDiskUsage(ctx context.Context) (string, error) {
 
 	// take an estimated percent of extra storage for safety & taking metadata into account.
 	// convert bytes to Gib
-	totalDiskUsageInGib := (totalDiskUsageInBytes + (totalDiskUsageInBytes*float64(diskUsageEstimateThreshold))/100) / float64(1024*1024*1024)
-	totalDiskUsageInString := fmt.Sprintf("%f", totalDiskUsageInGib) + "Gi"
+	totalDiskUsageInMi := (totalDiskUsageInBytes + (totalDiskUsageInBytes*float64(diskUsageEstimateThreshold))/100) / float64(1024*1024)
+	totalDiskUsageInString := fmt.Sprintf("%f", totalDiskUsageInMi) + "Mi"
 
 	return totalDiskUsageInString, nil
 }
