5bbc11f2e99c67a7d7c5a6fba0ad6a73b8caa58a
Pritam <pritam@appscode.com>
2023-07-25 14:36:53 +0600

    Just testing 4
    Signed-off-by: Pritam <pritam@appscode.com>

--- elasticsearch/es_client_v8.go
+++ elasticsearch/es_client_v8.go
@@ -329,7 +329,7 @@ func (es *ESClientV8) GetDBUserRole(ctx context.Context) (error, bool) {
 
 func (es *ESClientV8) CreateDBUserRole(ctx context.Context) error {
 	userRoleReqStruct := UserRoleReq{
-		[]string{"^(?!security_manager$)[a-z_]+$"},
+		[]string{},
 		[]DBPrivileges{
 			{
 				[]string{PrivilegeIndexAny},
