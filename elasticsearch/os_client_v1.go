a09d5ffa60347514a3cd39f9a6b0c96e3b6725fa
Pritam <pritam@appscode.com>
2023-10-04 18:48:30 +0600

    Fix os v1 for reconfigure tls.
    Signed-off-by: Pritam <pritam@appscode.com>

--- elasticsearch/os_client_v1.go
+++ elasticsearch/os_client_v1.go
@@ -111,7 +111,9 @@ func (os *OSClientV1) ClusterStatus() (string, error) {
 	if err2 := json.NewDecoder(res.Body).Decode(&response); err2 != nil {
 		return "", errors.Wrap(err2, "failed to parse the response body")
 	}
+	klog.Info("response------------->", response)
 	if value, ok := response["status"]; ok {
+		klog.Info("value------------->", value)
 		if strValue, ok := value.(string); ok {
 			return strValue, nil
 		}
