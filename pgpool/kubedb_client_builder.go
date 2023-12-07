add5d7e239a9a1c3407974153ef21dc0b03fe321
MobarakHsn <mobarak@appscode.com>
2023-12-07 16:35:43 +0600

    Pgpool db-client
    Signed-off-by: MobarakHsn <mobarak@appscode.com>

---
+++ pgpool/kubedb_client_builder.go
@@ -0,0 +1,179 @@
+/*
+Copyright AppsCode Inc. and Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package pgpool
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	core "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/klog/v2"
+	appbinding "kmodules.xyz/custom-resources/apis/appcatalog/v1alpha1"
+	api "kubedb.dev/pgpool/api/v1alpha2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"xorm.io/xorm"
+)
+
+const (
+	DefaultPgpoolDB = "postgres"
+)
+
+type KubeDBClientBuilder struct {
+	kc       client.Client
+	db       *api.Pgpool
+	url      string
+	podName  string
+	pgpoolDB string
+	ctx      context.Context
+}
+
+func NewKubeDBClientBuilder(kc client.Client, db *api.Pgpool) *KubeDBClientBuilder {
+	return &KubeDBClientBuilder{
+		kc: kc,
+		db: db,
+	}
+}
+
+func (o *KubeDBClientBuilder) WithURL(url string) *KubeDBClientBuilder {
+	o.url = url
+	return o
+}
+
+func (o *KubeDBClientBuilder) WithPod(podName string) *KubeDBClientBuilder {
+	o.podName = podName
+	return o
+}
+
+func (o *KubeDBClientBuilder) WithPgpoolDB(pgDB string) *KubeDBClientBuilder {
+	o.pgpoolDB = pgDB
+	return o
+}
+
+func (o *KubeDBClientBuilder) WithContext(ctx context.Context) *KubeDBClientBuilder {
+	o.ctx = ctx
+	return o
+}
+
+func (o *KubeDBClientBuilder) GetPgpoolXormClient() (*XormClient, error) {
+	if o.ctx == nil {
+		o.ctx = context.Background()
+	}
+
+	connector, err := o.getConnectionString()
+	if err != nil {
+		return nil, err
+	}
+
+	engine, err := xorm.NewEngine("postgres", connector)
+	if err != nil {
+		return nil, err
+	}
+	_, err = engine.Query("SELECT 1")
+	if err != nil {
+		return nil, err
+	}
+
+	engine.SetDefaultContext(o.ctx)
+	return &XormClient{
+		engine,
+	}, nil
+}
+
+func (o *KubeDBClientBuilder) GetPgpoolClient() (*Client, error) {
+	if o.ctx == nil {
+		o.ctx = context.Background()
+	}
+
+	connector, err := o.getConnectionString()
+	if err != nil {
+		return nil, err
+	}
+
+	// connect to database
+	db, err := sql.Open("postgres", connector)
+	if err != nil {
+		return nil, err
+	}
+
+	// ping to database to check the connection
+	if err := db.PingContext(o.ctx); err != nil {
+		closeErr := db.Close()
+		if closeErr != nil {
+			klog.Errorf("Failed to close client. error: %v", closeErr)
+		}
+		return nil, err
+	}
+
+	return &Client{db}, nil
+}
+
+func (o *KubeDBClientBuilder) getURL() string {
+	return fmt.Sprintf("%s.%s.%s.svc", o.podName, o.db.GoverningServiceName(), o.db.Namespace)
+}
+
+func (o *KubeDBClientBuilder) getPgpoolAuthCredentials() (string, string, error) {
+	db := o.db
+	var secretName string
+	if db.Spec.Backend != nil {
+		apb := &appbinding.AppBinding{}
+		err := o.kc.Get(o.ctx, types.NamespacedName{
+			Name:      db.Spec.Backend.Name,
+			Namespace: db.Namespace,
+		}, apb)
+		if err != nil {
+			return "", "", err
+		}
+		if apb.Spec.Secret == nil {
+			return "", "", fmt.Errorf("No database secret")
+		}
+		secretName = apb.Spec.Secret.Name
+	}
+	var secret core.Secret
+	err := o.kc.Get(o.ctx, client.ObjectKey{Namespace: db.Namespace, Name: secretName}, &secret)
+	if err != nil {
+		return "", "", err
+	}
+	user, ok := secret.Data[core.BasicAuthUsernameKey]
+	if !ok {
+		return "", "", fmt.Errorf("DB root user is not set")
+	}
+	pass, ok := secret.Data[core.BasicAuthPasswordKey]
+	if !ok {
+		return "", "", fmt.Errorf("DB root password is not set")
+	}
+	return string(user), string(pass), nil
+}
+
+func (o *KubeDBClientBuilder) getConnectionString() (string, error) {
+	user, pass, err := o.getPgpoolAuthCredentials()
+	if err != nil {
+		return "", err
+	}
+
+	if o.podName != "" {
+		o.url = o.getURL()
+	}
+
+	if o.pgpoolDB == "" {
+		o.pgpoolDB = DefaultPgpoolDB
+	}
+	//TODO ssl mode is disable now need to work on this after adding tls support
+	connector := fmt.Sprintf("user=%s password=%s host=%s port=%d connect_timeout=10 dbname=%s sslmode=%s", user, pass, o.url, 9999, o.pgpoolDB, "disable")
+	return connector, nil
+}
