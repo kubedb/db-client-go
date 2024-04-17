33742df6980e58679f3ebe55ddbb82c7042a650d
Md. Ishtiaq Islam <ishtiaq@appscode.com>
2024-04-17 12:27:02 +0600

    Create spaces for dashboard
    Signed-off-by: Md. Ishtiaq Islam <ishtiaq@appscode.com>

--- elasticsearchdashboard/client.go
+++ elasticsearchdashboard/client.go
@@ -37,7 +37,7 @@ const (
 "observability-visualization", "search"]}`
 	SavedObjectsExportURL = "/api/saved_objects/_export"
 	SavedObjectsImportURL = "/api/saved_objects/_import"
-	ListSpacesURL         = "/api/spaces/space"
+	SpacesURL             = "/api/spaces/space"
 )
 
 type Client struct {
@@ -87,4 +87,14 @@ type ResponseBody struct {
 	Version map[string]interface{} `json:"version"`
 	Status  map[string]interface{} `json:"status"`
 	Metrics map[string]interface{} `json:"metrics"`
+}
+
+type Space struct {
+	Id               string   `json:"id"`
+	Name             string   `json:"name"`
+	Description      string   `json:"description,omitempty"`
+	Color            string   `json:"color,omitempty"`
+	Initials         string   `json:"initials,omitempty"`
+	DisabledFeatures []string `json:"disabledFeatures,omitempty"`
+	ImageUrl         string   `json:"imageUrl,omitempty"`
 }
