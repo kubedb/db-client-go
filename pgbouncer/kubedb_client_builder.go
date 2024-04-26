423d465ff2164eb40b4ff4063d0349d3056d016e
Hiranmoy Das Chowdhury <hiranmoy@appscode.com>
2024-04-26 10:45:07 +0600

    Signed-off-by: Hiranmoy Das Chowdhury <hiranmoy@appscode.com>

--- pgbouncer/kubedb_client_builder.go
+++ pgbouncer/kubedb_client_builder.go
@@ -18,42 +18,38 @@ package pgbouncer
 
 import (
 	"context"
-	"database/sql"
-	"errors"
 	"fmt"
-
+	"k8s.io/apimachinery/pkg/types"
+	appbinding "kmodules.xyz/custom-resources/apis/appcatalog/v1alpha1"
 	api "kubedb.dev/apimachinery/apis/kubedb/v1alpha2"
 
 	_ "github.com/lib/pq"
 	core "k8s.io/api/core/v1"
-	"k8s.io/klog/v2"
-	"kmodules.xyz/client-go/tools/certholder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"xorm.io/xorm"
 )
 
 const (
-	DefaultPgBouncerDB = "pgbouncer"
+	DefaultBackendDBName = "postgres"
+	DefaultPgBouncerPort = 5432
+	TLSModeDisable       = "disable"
 )
 
 type KubeDBClientBuilder struct {
-	kc          client.Client
-	db          *api.PgBouncer
-	url         string
-	podName     string
-	pgBouncerDB string
+	kc            client.Client
+	pgbouncer     *api.PgBouncer
+	url           string
+	podName       string
+	backendDBName string
+	ctx           context.Context
+	databaseRef   *api.Databases
 }
 
-func NewKubeDBClientBuilder(kc client.Client, db *api.PgBouncer) *KubeDBClientBuilder {
+func NewKubeDBClientBuilder(kc client.Client, pp *api.PgBouncer) *KubeDBClientBuilder {
 	return &KubeDBClientBuilder{
-		kc: kc,
-		db: db,
+		kc:        kc,
+		pgbouncer: pp,
 	}
-}
-
-func (o *KubeDBClientBuilder) WithPod(podName string) *KubeDBClientBuilder {
-	o.podName = podName
-	return o
 }
 
 func (o *KubeDBClientBuilder) WithURL(url string) *KubeDBClientBuilder {
@@ -61,119 +57,112 @@ func (o *KubeDBClientBuilder) WithURL(url string) *KubeDBClientBuilder {
 	return o
 }
 
-func (o *KubeDBClientBuilder) WithPgBouncerDB(pgDB string) *KubeDBClientBuilder {
-	o.pgBouncerDB = pgDB
+func (o *KubeDBClientBuilder) WithPod(podName string) *KubeDBClientBuilder {
+	o.podName = podName
 	return o
 }
 
-func (o *KubeDBClientBuilder) GetPgbouncerXormClient(ctx context.Context) (*XormClient, error) {
-	connector, err := o.getConnectionString(ctx)
+func (o *KubeDBClientBuilder) WithDatabaseRef(db *api.Databases) *KubeDBClientBuilder {
+	o.databaseRef = db
+	return o
+}
+
+func (o *KubeDBClientBuilder) WithPgBouncerDB(pgDB string) *KubeDBClientBuilder {
+	o.backendDBName = pgDB
+	return o
+}
+
+func (o *KubeDBClientBuilder) WithContext(ctx context.Context) *KubeDBClientBuilder {
+	o.ctx = ctx
+	return o
+}
+
+func (o *KubeDBClientBuilder) GetPgBouncerXormClient() (*XormClient, error) {
+	if o.ctx == nil {
+		o.ctx = context.Background()
+	}
+
+	connector, err := o.getConnectionString()
 	if err != nil {
 		return nil, err
 	}
 
-	engine, err := xorm.NewEngine("postgres", connector)
+	engine, err := xorm.NewEngine(DefaultBackendDBName, connector)
 	if err != nil {
-		return nil, fmt.Errorf("failed to generate pgbouncer client using connection string: %v", err)
+		return nil, err
 	}
-	_, err = engine.Query("SHOW HELP")
+	_, err = engine.Query("SELECT 1")
 	if err != nil {
-		return nil, fmt.Errorf("failed to run query: %v", err)
+		err = engine.Close()
+		if err != nil {
+			return nil, err
+		}
+		return nil, err
 	}
-	return &XormClient{engine}, nil
+
+	engine.SetDefaultContext(o.ctx)
+	return &XormClient{
+		engine,
+	}, nil
 }
 
 func (o *KubeDBClientBuilder) getURL() string {
-	return fmt.Sprintf("%s.%s.%s.svc", o.podName, o.db.GoverningServiceName(), o.db.Namespace)
+	return fmt.Sprintf("%s.%s.%s.svc", o.podName, o.pgbouncer.GoverningServiceName(), o.pgbouncer.Namespace)
 }
 
-func (o *KubeDBClientBuilder) getPgBouncerAuthCredentials(ctx context.Context) (string, string, error) {
-	if o.db.Spec.AuthSecret == nil {
-		return "", "", errors.New("no database secret")
+func (o *KubeDBClientBuilder) getBackendAuth() (string, string, error) {
+
+	db := o.databaseRef
+
+	if db == nil || &db.DatabaseRef == nil {
+		return "", "", fmt.Errorf("there is no Database found for pgBouncer %s/%s", o.pgbouncer.Namespace, o.pgbouncer.Name)
 	}
-	var secret core.Secret
-	err := o.kc.Get(ctx, client.ObjectKey{Namespace: o.db.Namespace, Name: o.db.Spec.AuthSecret.Name}, &secret)
+	appBinding := &appbinding.AppBinding{}
+	err := o.kc.Get(o.ctx, types.NamespacedName{
+		Name:      db.DatabaseRef.Name,
+		Namespace: db.DatabaseRef.Namespace,
+	}, appBinding)
 	if err != nil {
 		return "", "", err
 	}
-	return string(secret.Data[core.BasicAuthUsernameKey]), string(secret.Data[core.BasicAuthPasswordKey]), nil
+	if appBinding.Spec.Secret == nil {
+		return "", "", fmt.Errorf("backend postgres auth secret unspecified for pgBouncer %s/%s", o.pgbouncer.Namespace, o.pgbouncer.Name)
+	}
+
+	var secret core.Secret
+	err = o.kc.Get(o.ctx, client.ObjectKey{Namespace: appBinding.Namespace, Name: appBinding.Spec.Secret.Name}, &secret)
+	if err != nil {
+		return "", "", err
+	}
+
+	user, ok := secret.Data[core.BasicAuthUsernameKey]
+	if !ok {
+		return "", "", fmt.Errorf("error getting backend username")
+	}
+
+	pass, ok := secret.Data[core.BasicAuthPasswordKey]
+	if !ok {
+		return "", "", fmt.Errorf("error getting backend password")
+	}
+
+	return string(user), string(pass), nil
+
 }
 
-func (o *KubeDBClientBuilder) GetPgBouncerClient(ctx context.Context) (*Client, error) {
-	connector, err := o.getConnectionString(ctx)
+func (o *KubeDBClientBuilder) getConnectionString() (string, error) {
+	user, pass, err := o.getBackendAuth()
 	if err != nil {
-		return nil, err
-	}
-	// connect to database
-	db, err := sql.Open("postgres", connector)
-	if err != nil {
-		return nil, err
+		return "", err
 	}
 
-	// ping to database to check the connection
-	if _, err := db.QueryContext(ctx, "SHOW HELP;"); err != nil {
-		closeErr := db.Close()
-		if closeErr != nil {
-			klog.Errorf("Failed to close client. error: %v", closeErr)
-		}
-		return nil, err
-	}
-
-	return &Client{db}, nil
-}
-
-func (o *KubeDBClientBuilder) getConnectionString(ctx context.Context) (string, error) {
 	if o.podName != "" {
 		o.url = o.getURL()
 	}
-	dnsName := o.url
-	port := api.PgBouncerDatabasePort
 
-	if o.pgBouncerDB == "" {
-		o.pgBouncerDB = DefaultPgBouncerDB
+	if o.backendDBName == "" {
+		o.backendDBName = DefaultBackendDBName
 	}
-
-	user, pass, err := o.getPgBouncerAuthCredentials(ctx)
-	if err != nil {
-		return "", fmt.Errorf("DB basic auth is not found for PgBouncer %v/%v", o.db.Namespace, o.db.Name)
-	}
-	cnnstr := ""
-	sslMode := o.db.Spec.SSLMode
-	if sslMode == "" {
-		sslMode = api.PgBouncerSSLModeDisable
-	}
-
-	if o.db.Spec.TLS != nil {
-		paths, err := o.getTLSConfig(ctx)
-		if err != nil {
-			return "", err
-		}
-		if o.db.Spec.ConnectionPool.AuthType == api.PgBouncerClientAuthModeCert || o.db.Spec.SSLMode == api.PgBouncerSSLModeVerifyCA || o.db.Spec.SSLMode == api.PgBouncerSSLModeVerifyFull {
-			cnnstr = fmt.Sprintf("user=%s password=%s host=%s port=%d connect_timeout=15 dbname=%s sslmode=%s sslrootcert=%s sslcert=%s sslkey=%s", user, pass, dnsName, port, o.pgBouncerDB, sslMode, paths.CACert, paths.Cert, paths.Key)
-		} else {
-			cnnstr = fmt.Sprintf("user=%s password=%s host=%s port=%d connect_timeout=15 dbname=%s sslmode=%s sslrootcert=%s", user, pass, dnsName, port, o.pgBouncerDB, sslMode, paths.CACert)
-		}
-	} else {
-		cnnstr = fmt.Sprintf("user=%s password=%s host=%s port=%d connect_timeout=15 dbname=%s sslmode=%s", user, pass, dnsName, port, o.pgBouncerDB, sslMode)
-	}
-	return cnnstr, nil
-}
-
-func (o *KubeDBClientBuilder) getTLSConfig(ctx context.Context) (*certholder.Paths, error) {
-	secretName := o.db.GetCertSecretName(api.PgBouncerClientCert)
-
-	var certSecret core.Secret
-	err := o.kc.Get(ctx, client.ObjectKey{Namespace: o.db.Namespace, Name: secretName}, &certSecret)
-	if err != nil {
-		klog.Error(err, "failed to get certificate secret.", secretName)
-		return nil, err
-	}
-
-	certs, _ := certholder.DefaultHolder.ForResource(api.SchemeGroupVersion.WithResource(api.ResourcePluralPgBouncer), o.db.ObjectMeta)
-	paths, err := certs.Save(&certSecret)
-	if err != nil {
-		klog.Error(err, "failed to save certificate")
-		return nil, err
-	}
-	return paths, nil
+	//TODO ssl mode is disable now need to work on this after adding tls support
+	connector := fmt.Sprintf("user=%s password=%s host=%s port=%d connect_timeout=10 dbname=%s sslmode=%s", user, pass, o.url, DefaultPgBouncerPort, o.backendDBName, TLSModeDisable)
+	return connector, nil
 }
