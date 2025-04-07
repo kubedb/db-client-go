7c79366d25ff3e5945bbc09a4cbaf4bc12d557ff
Muhammad Raisul Islam Evan <raisul@appscode.com>
2025-04-07 12:33:57 +0600

    Fix add-2
    Signed-off-by: Muhammad Raisul Islam Evan <raisul@appscode.com>

--- ignite/kubedb_client_builder.go
+++ ignite/kubedb_client_builder.go
@@ -180,5 +180,5 @@ func (o *KubeDBClientBuilder) AlterUserPassword(sqlClient *sql.DB, password stri
 }
 
 func (o *KubeDBClientBuilder) Address() string {
-	return fmt.Sprintf("%s.%s.svc", o.db.ServiceName(), o.db.Namespace)
+	return fmt.Sprintf("%s.%s.svc.cluster.local", o.db.GoverningServiceName(), o.db.Namespace)
 }
