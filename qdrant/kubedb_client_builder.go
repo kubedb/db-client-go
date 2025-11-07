d584d256e337a3537f890aed486fb7f820ab2c8c
Masood <masood@appscode.com>
2025-11-07 15:19:30 +0600

    qdrant cleanup
    Signed-off-by: Masood <masood@appscode.com>

--- qdrant/kubedb_client_builder.go
+++ qdrant/kubedb_client_builder.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/qdrant/go-client/qdrant"
-	core "k8s.io/api/core/v1"
 	"kubedb.dev/apimachinery/apis/kubedb"
 	api "kubedb.dev/apimachinery/apis/kubedb/v1alpha2"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -29,36 +28,15 @@ func (o *KubeDBClientBuilder) WithContext(ctx context.Context) *KubeDBClientBuil
 	return o
 }
 
-func (o *KubeDBClientBuilder) GetQdrantAPIKey() string {
-	db := o.db
-	var secretName string
-	if db.Spec.AuthSecret != nil {
-		secretName = db.GetAuthSecretName()
-	}
-
-	var secret core.Secret
-	err := o.kc.Get(o.ctx, client.ObjectKey{Namespace: db.Namespace, Name: secretName}, &secret)
-	if err != nil {
-		return ""
-	}
-
-	apiKey, ok := secret.Data[kubedb.QdrantAPIKey]
-	if !ok {
-		return ""
-	}
-
-	return string(apiKey)
-}
-
 func (o *KubeDBClientBuilder) GetQdrantClient() (*qdrant.Client, error) {
 	if o.ctx == nil {
 		o.ctx = context.Background()
 	}
 
 	config := &qdrant.Config{
-		Host:   o.db.PrimaryServiceDNS(),
+		Host:   o.db.ServiceDNS(),
 		Port:   kubedb.QdrantGRPCPort,
-		APIKey: o.GetQdrantAPIKey(),
+		APIKey: o.db.GetAPIKey(o.ctx, o.kc),
 	}
 
 	qdrantClient, err := qdrant.NewClient(config)
