59b6a2a31b138fccc567a271863c3135763560d0
ZobayerAbedin <zobayerabedin@gmail.com>
2025-12-05 17:26:06 +0600

    update apimachinery version
    Signed-off-by: ZobayerAbedin <zobayerabedin@gmail.com>

--- vendor/kubedb.dev/apimachinery/apis/kubedb/v1alpha2/oracle_helpers.go
+++ vendor/kubedb.dev/apimachinery/apis/kubedb/v1alpha2/oracle_helpers.go
@@ -294,6 +294,13 @@ func (o *Oracle) SetDefaults(kc client.Client) {
 		o.Spec.StorageType = StorageTypeDurable
 	}
 
+	if o.Spec.AuthSecret == nil {
+		o.Spec.AuthSecret = &SecretReference{}
+	}
+	if o.Spec.AuthSecret.Kind == "" {
+		o.Spec.AuthSecret.Kind = kubedb.ResourceKindSecret
+	}
+
 	o.SetListenerDefaults()
 	o.initializePodTemplates()
 
