2cddc315ec007f41447dda7fc53bcf492544e599
obaydullahmhs <obaydullah@appscode.com>
2025-12-05 17:47:15 +0600

    Add db2 client go
    Signed-off-by: obaydullahmhs <obaydullah@appscode.com>

---
+++ db2/client.go
@@ -0,0 +1,44 @@
+package db2
+
+import (
+	"net/http"
+
+	"github.com/go-resty/resty/v2"
+	"k8s.io/klog/v2"
+)
+
+type Client struct {
+	*resty.Client
+	Config *Config
+}
+
+type Config struct {
+	host             string
+	api              string
+	username         string
+	password         string
+	connectionScheme string
+	transport        *http.Transport
+}
+
+func (cc *Client) PingDB2() (bool, error) {
+	req := cc.Client.R().SetDoNotParseResponse(true)
+	res, err := req.Get("/ready")
+	if err != nil {
+		klog.Error(err, "Failed to send http request")
+		return false, err
+	}
+
+	return res.StatusCode() == http.StatusOK, nil
+}
+
+func (cc *Client) ReadWriteCheck() (bool, error) {
+	req := cc.Client.R().SetDoNotParseResponse(true)
+	res, err := req.Get("/test")
+	if err != nil {
+		klog.Error(err, "Failed to send http request")
+		return false, err
+	}
+
+	return res.StatusCode() == http.StatusOK, nil
+}
