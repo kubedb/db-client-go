64a445ce999d9f9dd472554ee9ea07a2ee1e8c7a
sabnaj <sabnaj@appscode.com>
2026-01-14 18:22:03 +0600

    changed in StartHadr
    Signed-off-by: sabnaj <sabnaj@appscode.com>

--- db2/client.go
+++ db2/client.go
@@ -2,6 +2,7 @@ package db2
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/go-resty/resty/v2"
 	"k8s.io/klog/v2"
@@ -43,10 +44,12 @@ func (cc *Client) ReadWriteCheck() (bool, error) {
 	return res.StatusCode() == http.StatusOK, nil
 }
 
-func (cc *Client) ConfigurePrimary(dbName string) (bool, error) {
+func (cc *Client) ConfigurePrimary(dbName string, primaryPod int, standbyPod int) (bool, error) {
 	req := cc.Client.R().
 		SetDoNotParseResponse(true).
-		SetQueryParam("dbName", dbName)
+		SetQueryParam("dbName", dbName).
+		SetQueryParam("primaryPod", strconv.Itoa(primaryPod)).
+		SetQueryParam("standbyPod", strconv.Itoa(standbyPod))
 
 	res, err := req.Get("/primary")
 	if err != nil {
@@ -56,12 +59,26 @@ func (cc *Client) ConfigurePrimary(dbName string) (bool, error) {
 	return res.StatusCode() == http.StatusOK, nil
 }
 
-func (cc *Client) ConfigureStandby(dbName string) (bool, error) {
+func (cc *Client) ConfigureStandby(dbName string, primaryPod int, standbyPod int) (bool, error) {
+	req := cc.Client.R().
+		SetDoNotParseResponse(true).
+		SetQueryParam("dbName", dbName).
+		SetQueryParam("primaryPod", strconv.Itoa(primaryPod)).
+		SetQueryParam("standbyPod", strconv.Itoa(standbyPod))
+
+	res, err := req.Get("/standby")
+	if err != nil {
+		klog.Error(err, "Failed to send http request")
+		return false, err
+	}
+	return res.StatusCode() == http.StatusOK, nil
+}
+func (cc *Client) StartHADR(dbName string) (bool, error) {
 	req := cc.Client.R().
 		SetDoNotParseResponse(true).
 		SetQueryParam("dbName", dbName)
 
-	res, err := req.Get("/standby")
+	res, err := req.Get("/starthadr")
 	if err != nil {
 		klog.Error(err, "Failed to send http request")
 		return false, err
