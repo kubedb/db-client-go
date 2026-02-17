17588e7dd4ceabf6660a42ad00851c5f0246b867
Fazle Rabbi Sarker <fazlerabbi@appscode.com>
2026-02-17 12:14:18 +0600

    update for tls
    Signed-off-by: Fazle Rabbi Sarker <fazlerabbi@appscode.com>

--- neo4j/kubedb_client_builder.go
+++ neo4j/kubedb_client_builder.go
@@ -115,7 +115,7 @@ func (o *KubeDBClientBuilder) GetNeo4jClient() (*Client, error) {
 
 	tlsConfig := &tls.Config{}
 
-	if o.db.Spec.TLS != nil && o.db.Spec.TLS.IssuerRef != nil {
+	if o.db.Spec.TLS != nil && o.db.Spec.TLS.IssuerRef != nil && *o.db.Spec.TLS.Bolt.Enabled {
 		certSecret := &core.Secret{}
 		err := o.kc.Get(o.ctx, types.NamespacedName{
 			Namespace: o.db.Namespace,
@@ -197,7 +197,7 @@ func (c *Client) ExecuteQuery(ctx context.Context, query string, params map[stri
 func (o *KubeDBClientBuilder) buildConnectionURL() string {
 	scheme := "neo4j"
 
-	if o.db.Spec.TLS != nil && o.db.Spec.TLS.IssuerRef != nil {
+	if o.db.Spec.TLS != nil && o.db.Spec.TLS.IssuerRef != nil && *o.db.Spec.TLS.Bolt.Enabled {
 		scheme = "neo4j+s"
 	}
 
